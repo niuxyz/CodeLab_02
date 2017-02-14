204999ae2bed8e62a26c7583e9ff682b60f47cc3
niuxyz <sc5875@nyu.edu>
2017-02-13 21:18:20 -0500

    week3 hw
    week3 hw code

---
+++ sc5875_codelab1_wk3hw/Assets/scripts/TriggerBox.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBox : MonoBehaviour {
+	public Wk3GameManager sceneManager;
+	public string SceneName;
+
+	//when something enter the trigger zone
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.name == "Player") {
+			sceneManager.loadAnotherScene (SceneName);
+		}
+	}
+
+	/* //When something in the trigger zone
+	void OnTriggerStay2D(Collider2D collider)
+	{
+		
+	}
+		//When something leave the trigger zone
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		
+	}
+	*/
+}
