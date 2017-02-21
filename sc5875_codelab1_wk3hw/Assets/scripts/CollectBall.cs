f99fb5e466d81b4401b53ea801556719079854f1
niuxyz <sc5875@nyu.edu>
2017-02-21 18:46:35 -0500

    Week4_hw03
    Score manerger, score counts, apple counts script works with score.

--- sc5875_codelab1_wk3hw/Assets/scripts/CollectBall.cs
+++ sc5875_codelab1_wk3hw/Assets/scripts/CollectBall.cs
@@ -2,20 +2,25 @@
 using System.Collections;
 
 public class CollectBall : MonoBehaviour {
-	public int	appleCount = 0;
+	//public int	appleCount = 0;
+	private ScoreManager scoremanager;
 
-
+	void Start(){
+		scoremanager = GetComponent<ScoreManager> ();
+	}
 	// Use this for initialization
 	void OnTriggerEnter2D (Collider2D apple) {
 		if (apple.gameObject.tag == "apple") {
-			appleCount++;
+			//appleCount++;
+			scoremanager.Score += 6;
 		}
 	}
 	
 	void OnTriggerExit2D(Collider2D apple)
 	{
 		if (apple.gameObject.tag == "apple") {
-			appleCount--;
+			//appleCount--;
+			scoremanager.Score -= 6;
 		}
 	}
 }
---
+++ sc5875_codelab1_wk3hw/Assets/scripts/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	private const int highScore = 10;
+	private int score;
+
+	public int Score {
+		get{
+			return score;
+		}
+		set{
+			score = value;
+			if (score > highScore) {
+				score = highScore;
+			}
+		}
+	}
+
+	void Start()
+	{
+		score = 0;
+		Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Score);
+	}
+
+	/*
+	public int getScore()
+	{
+		return Score;
+	}
+
+
+	public void setScore(int _Score)
+	{
+		Score = _Score;
+	}
+	*/
+}
